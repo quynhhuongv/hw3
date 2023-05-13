b5272255bac28c758e7a34f76e46cb1001ac0201
Huong Vu <huong.vu@kellogg.northwestern.edu>
2023-05-13 16:14:05 +0000

    Commit Please

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
 # end
 
 Rails.application.routes.draw do
   resources "places"
   resources "posts"
+  get("/", { :controller => "places", :action => "index" })
 end
